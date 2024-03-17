70e5bd9b7d4651ced79c03a4e49073c799f0d487
Éder Casagranda <casagrandaeder461@gmail.com>
2024-03-16 22:25:26 -0300

    calculator

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function somar(){
+    var valor1 = parseFloat(document.getElementById('valor1').value)
+    var valor2 = parseFloat(document.getElementById('valor2').value)
+
+    document.getElementById('resultado').innerHTML = valor1 + valor2
+}
